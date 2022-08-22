45cbfb01dd9105fc25eedc4ab8dfab1c192793da
anshul bansal <akbt.tech17@gmail.com>
2022-08-22 12:39:38 +0530

    Created a context object in Model Created first get api

--- DemoWebApi/Controllers/DeptController.cs
+++ DemoWebApi/Controllers/DeptController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using DemoWebApi.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DemoWebApi.Controllers
 {
@@ -7,6 +10,34 @@ namespace DemoWebApi.Controllers
     [ApiController]
     public class DeptController : ControllerBase
     {
+        db1045Context db = new db1045Context();
 
+        [HttpGet]
+        [Route("listdept")]
+        public IActionResult GetDept()
+        {
+            // EF common syntax
+            //var data = db.Depts.ToList(); 
+
+            // Linq Syntax
+            //var data = from dept in db.Depts select dept;
+
+            // suppose you have 20 cols, but we dont want to return all 20 columns
+            var data = from dept in db.Depts select new { Id = dept.Id, Name = dept.Name, Location = dept.Location };
+
+            return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("listdept/{id}")]
+        public IActionResult GetDept(int? id)
+        {
+            if (id == null) {
+                return BadRequest("Id is NUll");
+            }
+
+            var data = from dept in db.Depts where dept.Id == id select new { Id = dept.Id, Name = dept.Name, Location = dept.Location };
+            return Ok(data);
+        }
     }
 }
